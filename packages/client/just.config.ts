91a816922238e4320f742c3700165de94c6207a6
Ken Chau <ken@gizzar.com>
2021-05-08 20:22:00 -0700

    live fix fetched stuff

--- packages/client/just.config.ts
+++ packages/client/just.config.ts
@@ -1,17 +1,19 @@
 import { graphqlCodeGenTask } from "monorepo-scripts/tasks/graphql";
 import { transpile } from "monorepo-scripts/just.config";
-import { webpackDevServerTask } from "monorepo-scripts";
+import { webpackDevServerTask, parallel } from "monorepo-scripts";
 
-export const generate = graphqlCodeGenTask({
-  schema: require.resolve("data"),
-  documents: "./src/**/*.+(graphql|gql)",
-  generates: {
-    [process.cwd() + "/src/generated/query.ts"]: {
-      plugins: ["typescript", "typescript-operations", "typescript-react-query"],
+export const generate = (watch: boolean = false) =>
+  graphqlCodeGenTask({
+    schema: require.resolve("data"),
+    documents: "./src/**/*.+(graphql|gql)",
+    generates: {
+      [process.cwd() + "/src/generated/query.ts"]: {
+        plugins: ["typescript", "typescript-operations", "typescript-react-query"],
+      },
     },
-  },
-});
+    watch
+  });
 
 export const build = transpile;
 
-export const dev = webpackDevServerTask()
+export const dev = parallel(generate(true), webpackDevServerTask());
